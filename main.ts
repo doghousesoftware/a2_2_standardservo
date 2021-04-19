cb49e4be190723e8446ec7f36fb5cdb32abad0eb
bryan weinstein <bryanweinstein@outlook.com>
2021-04-19 13:45:45 -0700

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,12 @@
 input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.Heart)
-    pins.servoWritePin(AnalogPin.P2, 180)
+    servos.P0.setAngle(180)
     basic.pause(1000)
+    servos.P0.stop()
 })
 input.onButtonPressed(Button.B, function () {
     basic.showIcon(IconNames.No)
-    pins.servoWritePin(AnalogPin.P2, 0)
+    servos.P0.setAngle(0)
     basic.pause(1000)
+    servos.P0.stop()
 })
